7a030e52607b50fb209dc59d0fad43645dd325db
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-11-30 16:34:51 +0200

    U 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,10 @@ if __name__ == "__main__":
     if length == 1:
         print("0 arguments.")
     elif length == 2:
-        print("1 arguments:")
+        print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(length - 1))
         for i, args in enumerate(sys.argv[1:], start=1):
             print("{}: {}".format(i, args))
+    
