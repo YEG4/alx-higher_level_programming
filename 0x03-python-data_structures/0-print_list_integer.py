247fd889cfeababacbdaea9c8cdd27c76cca218f
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-12-04 11:03:25 +0200

    UUU

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    def print_list_integer(mylist=[]):
-        for i in range(len(mylist)):
-            print("{:d}".format(mylist[i]))
+def print_list_integer(mylist=[]):
+    for i in range(len(mylist)):
+        print("{:d}".format(mylist[i]))
