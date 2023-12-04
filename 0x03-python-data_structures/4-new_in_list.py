6345aa095d69f63b00d8a9e3d77edfdd52f15874
YEG4 <ahmed.t.ghoneim@gmail.com>
2023-12-04 11:47:51 +0200

    Y

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,5 +5,6 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
-        a = my_list.copy()[idx] = element
+        a = my_list.copy()
+        a[idx] = element
         return a
