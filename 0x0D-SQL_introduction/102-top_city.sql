b86bae499f3df686a63dd9410a7e018b6db99ca8
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-21 17:46:12 +0300

    101-avg_temperatures.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,7 @@
 -- SQL script to computes the average of temp for each city.
-SELECT TOP(3) city , AVG(value) AS avg_temp 
+SELECT  city , AVG(value) AS avg_temp 
 FROM temperatures
 WHERE month = 7 OR month = 8
 GROUP BY city
-ORDER BY value DESC;
+ORDER BY value DESC
+LIMIT 3;
