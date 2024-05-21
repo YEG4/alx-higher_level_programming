31a98d419e2e0bb3138d8887b587368dca58b0ee
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-21 16:49:04 +0300

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,5 @@
 -- SQL script to convert a database to utf8(collate utf8mb4_unicode_ci)
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+
