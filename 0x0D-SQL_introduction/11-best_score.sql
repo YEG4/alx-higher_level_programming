ca94053dcf5dede96823251637e6bf977b21a021
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-21 16:27:48 +0300

    Added 12-no_cheating.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
--- SQL script to list records for two columns.
+-- SQL script to list records for two columns where score is greater than 10.
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- SQL script to Update a record for a give id.
+UPDATE second_table SET score=10 WHERE name='Bob';
