2120389a72f7c9de50a811bb8ea9aefa792bb2b9
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-22 17:56:08 +0300

    9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- SQL script to list all cities in the database
 
-SELECT cities.id as id, cities.name as name, states.name as name
+SELECT cities.id AS id, cities.name AS name, states.name AS name
 FROM cities
 JOIN states
 ON cities.state_id = states.id
