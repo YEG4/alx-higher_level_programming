c4027fa93a07f0368db04b522b51085866ae35c9
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-23 00:26:53 +0300

    15-comedy_only.sql

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,10 @@
+-- SQL script to list all shows of the genre Comedy
+
+SELECT s.title AS 'name'
+FROM tv_genres AS g
+JOIN tv_show_genres AS sg
+ON g.id = sg.genre_id
+JOIN tv_shows AS s
+ON	s.id = sg.show_id
+WHERE g.name = 'Comedy'
+ORDER BY s.title;
