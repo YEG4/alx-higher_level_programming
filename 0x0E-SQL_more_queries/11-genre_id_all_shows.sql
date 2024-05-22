772382cbe87ced0dacc75d549cd89595c3340937
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-22 23:07:57 +0300

    11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- SQL script to list all shows that have atleast one genre linked
+
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_show_genres
+LEFT JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
