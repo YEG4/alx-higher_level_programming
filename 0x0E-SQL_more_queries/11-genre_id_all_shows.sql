60f36bf37afc6d3c0b5f9e0fc4ff85d81238f55b
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-22 23:17:16 +0300

    11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,8 +2,6 @@
 
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_show_genres
-LEFT JOIN tv_shows
+RIGHT OUTER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-LEFT JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id; 
