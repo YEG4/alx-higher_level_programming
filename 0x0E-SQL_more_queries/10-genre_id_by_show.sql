920dde6a6b48aa0a6f72636426faa983daedb6b3
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-22 23:06:10 +0300

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,10 +1,9 @@
 -- SQL script to list all shows that have atleast one genre linked
 
-SELECT tv_shows.title AS title, tv_show_genres.id AS genre_id
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_show_genres
 JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY (tv_show_genres.genre_id)
-WHERE COUNT(tv_show_genres.genre_id) >= 1;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
