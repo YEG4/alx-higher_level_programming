ee30c7de90e4a86a969a35f931a2687461bf433d
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-22 16:21:02 +0300

    modified 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,12 +1,12 @@
 -- SQL script to list all privileges for users
 -- Check grants for user_0d_1
-SELECT * FROM mysql.user WHERE User = 'user_0d_1' AND Host = 'localhost';
+-- SELECT * FROM mysql.user WHERE User = 'user_0d_1' AND Host = 'localhost';
 
 -- Attempt to show grants for user_0d_1 (requires SHOW GRANTS privilege)
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
 -- Check grants for user_0d_2
-SELECT * FROM mysql.user WHERE User = 'user_0d_2' AND Host = 'localhost';
+-- SELECT * FROM mysql.user WHERE User = 'user_0d_2' AND Host = 'localhost';
 
 -- Attempt to show grants for user_0d_2 (requires SHOW GRANTS privilege)
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
