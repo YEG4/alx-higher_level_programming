3bea079861b33dde5ca529bd6c0f494563f78d38
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-05-22 17:25:31 +0300

    7-cities.sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- SQL Script to create a table within a database
+
+-- Create Database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id));
